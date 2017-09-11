452bf599d78801059e399abb360a36cdcd867784
Anton <Anton1qw2@yandex.ru>
2017-09-11 18:21:25 +0300

    Simple UI

--- Logic.py
+++ Logic.py
@@ -1,57 +1,58 @@
 import cv2
 import numpy as np
+class Logic:
+    def creategrid(img, thickness, count, color):
+        h,w,c = img.shape
+        h_2, w_2 = int(h/count), int(w/count)
+        for i in range(count-1):
+            cv2.line(img, (w_2 + i*(w_2), 0), (w_2 + i*(w_2), h), color, thickness)
+        for i in range(count-1):
+            cv2.line(img, (0, h_2 + i*(h_2)), (w, h_2 + i*(h_2)), color, thickness)
+        return img
 
-def creategrid(img, thickness, count, color):
-    h,w,c = img.shape
-    h_2, w_2 = int(h/count), int(w/count)
-    for i in range(count-1):
-        cv2.line(img, (w_2 + i*(w_2), 0), (w_2 + i*(w_2), h), color, thickness)
-    for i in range(count-1):
-        cv2.line(img, (0, h_2 + i*(h_2)), (w, h_2 + i*(h_2)), color, thickness)
-    return img
+    def findgrid(img):
+        h, w, c = img.shape
+        img_output = np.zeros((h, w, 3), np.uint8)
+        img_output[0:h,0:w] = (255,255,255)
 
-def findgrid(img):
-    h, w, c = img.shape
-    img_output = np.zeros((h, w, 3), np.uint8)
-    img_output[0:h,0:w] = (255,255,255)
+        img_edge = cv2.Canny(img, 10, 50)
 
-    img_edge = cv2.Canny(img, 100, 150)
-
-    x, y = 0, 0
-    for i in range(h):
         x, y = 0, 0
-        for j in range(w):
-            if img_edge[i][j] == 255:
-                x = x + 1
-            else:
-                y = y + 1
-        if x == 0:
-            for k in (i-1, i, i+1):
-                img_output[k, 0:w] = img[k, 0:w]
+        for i in range(h):
+            x, y = 0, 0
+            for j in range(w):
+                if img_edge[i][j] == 255:
+                    x = x + 1
+                else:
+                    y = y + 1
+            if x == 0:
+                for k in (i-1, i, i+1):
+                    img_output[k, 0:w] = img[k, 0:w]
 
-    for i in range(w):
-        x, y = 0, 0
-        for j in range(h):
-            if img_edge[j][i] == 255:
-                x = x + 1
-            else:
-                y = y + 1
-        if x == 0:
-            for k in (i-1, i, i+1):
-                img_output[0:h, k] = img[0:h, k]
+        for i in range(w):
+            x, y = 0, 0
+            for j in range(h):
+                if img_edge[j][i] == 255:
+                    x = x + 1
+                else:
+                    y = y + 1
+            if x == 0:
+                for k in (i-1, i, i+1):
+                    img_output[0:h, k] = img[0:h, k]
 
-    cv2.imshow('456', img - img_output)
-    return img_output
+        cv2.imshow('456', img - img_output)
+        return img_output
+'''
+    img_input = cv2.imread('C:/Users/Natali/Documents/145274.jpg')
 
-img_input = cv2.imread('C:/Users/Natali/Documents/145274.jpg')
+    thickness = 8
+    count = 6
+    color = (0, 125, 0)
+    img_grid = creategrid(img_input, thickness, count, color)
+    cv2.imshow('123', img_grid)
 
-thickness = 8
-count = 6
-color = (0, 125, 0)
-img_grid = creategrid(img_input, thickness, count, color)
-cv2.imshow('123', img_grid)
+    img_only_grid = findgrid(img_grid)
+    cv2.imshow('234', img_only_grid)
 
-img_only_grid = findgrid(img_grid)
-cv2.imshow('234', img_only_grid)
-
-cv2.waitKey(0)
+    cv2.waitKey(0)
+'''
--- UI.py
+++ UI.py
@@ -1,23 +1,67 @@
+from Labs import Logic
 import cv2
 import tkinter as tk
 from PIL import Image, ImageTk
+from tkinter.filedialog import askopenfile as fo
 def printer (event):
     print ("hello world")
+def cvtotk(a):
+    alo = cv2.cvtColor(a, cv2.COLOR_BGR2RGB)
+    alo = Image.fromarray(alo)
+    alo = ImageTk.PhotoImage(alo)
+    return alo
+def selectimage(event):
+    d = fo()
+    print(d.name)
+    acv = cv2.imread(d.name)
+    a= cvtotk(cv2.resize(acv, (400, 400)))
+    leftwindow.create_image(0, 0, image=a, anchor="nw")
+    leftwindow.pack(side="right")
+    root.mainloop()
+def creategird(event):
+    a= Logic.Logic.creategrid(acv, 10, 5, color =(0,125,0))
+    a = cv2.resize(a, (400, 400))
+    a= cvtotk(a)
+    rightwindow.create_image(0, 0, image=a, anchor="nw")
+    rightwindow.pack(side="right")
+    root.mainloop()
+    print("Решетка")
+def findgird(event):
+#    cv2.imshow('234', acv)
+#    cv2.waitKey(0)
+    a = Logic.Logic.findgrid(acv)
+    a = cv2.resize(a, (400, 400))
+    a= cvtotk(a)
+    rightwindow.create_image(0, 0, image=a, anchor="nw")
+    rightwindow.pack(side="right")
+    root.mainloop()
+    print("Нашли решетку")
+
+
 root = tk.Tk()
-a= cv2.imread('/home/anton/Downloads/345.jpg')
-grid = tk.Button(root)
+acv= cv2.imread('Stock.jpg')
+#Настройка кнопок
+selectim = tk.Button(root, text = "Выберете изображение")
+selectim.bind("<Button-1>", selectimage)
+selectim.pack()
+crtgird = tk.Button(root, text ="Создать решетку")
+crtgird.bind("<Button-1>", creategird )
+crtgird.pack()
+fndgird = tk.Button(root, text = "Найти решетку")
+fndgird.bind("<Button-1>",findgird)
+fndgird.pack()
 
-grid.bind("<Button-1>", printer)
-grid.pack()
 root.resizable
-c = tk.Canvas(root, width=300, height = 500)
+leftwindow = tk.Canvas(root, width=400, height = 400)
+rightwindow = tk.Canvas(root, width = 400, height= 400)
 #a = Image.open(a)
-a = cv2.cvtColor(a, cv2.COLOR_BGR2RGB)
-a = Image.fromarray(a)
-a = ImageTk.PhotoImage(a)
-c.create_image(0,0, image=a,anchor ="nw")
-c.pack()
-tk.Label(root, text = "/home/anton/Downloads/345.jpg").pack()
-
+a = cvtotk(cv2.resize(acv, (400, 400) ))
+rightwindow.create_image(0,0, image =a, anchor = "nw")
+rightwindow.addtag_all('im')
+rightwindow.pack(side = "right")
+leftwindow.create_image(0,0, image=a,anchor ="nw")
+leftwindow.addtag_all('im')
+leftwindow.pack(side = "left")
+#tk.Label(root, text = "/home/anton/Downloads/345.jpg").pack()
 root.mainloop()
 
